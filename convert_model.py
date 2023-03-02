0bdcfba8e620325af68b303c108bbab52ad4ebbd
seok436 <seok7302@gmail.com>
2023-03-02 16:21:50 +0900

    DepthRepVGGNet & minor update

--- convert_model.py
+++ convert_model.py
@@ -1,14 +1,35 @@
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from functools import partial
+from collections import OrderedDict
 from networks.depth.DepthResNet import DepthResDNet
+from networks.depth.DepthRepVGGNet import DepthRepVGGNet
 
 
 def main():
-    model = DepthResDNet("18np")
-    print('done')
-
+    # model = DepthResDNet("18np")
+    main_folder = '/home/seok436/tmp'
+    model_name = 'repVGG_model_pretrained_b12_e30_separate_resnet'
+    epoch = 30
+    load_path = os.path.join(os.path.join(main_folder, model_name), 'models/weights_{}'.format(epoch-1))
+    print(load_path)
+    model = DepthRepVGGNet("18np")
+    encoder = torch.load(os.path.join(load_path, 'encoder.pth'))
+    decoder = torch.load(os.path.join(load_path, 'depth.pth'))
+    endecoder = OrderedDict()
+    for k, v in encoder.items():
+        if k == 'height' or k == 'width' or k == 'use_stereo':
+            continue
+        endecoder['encoder.' + k] = v
+    for k, v in decoder.items():
+        if k in endecoder:
+            endecoder['decoder.' + k] += v
+        else:
+            endecoder['decoder.' + k] = v
+    model.load_state_dict(endecoder)
+    torch.save({'state_dict': model.state_dict()}, os.path.join(main_folder, '{}.ckpt'.format(model_name)))
 
 if __name__ == '__main__':
     # args = parse_args()
---
+++ networks/depth/DepthRepVGGNet.py
@@ -0,0 +1,64 @@
+from __future__ import absolute_import, division, print_function
+from collections import OrderedDict
+from functools import partial
+from layers import *
+import torch.utils.checkpoint as checkpoint
+import networks.repVGG as RepVGG
+from ..repVGG_encoder import RepVGGencoder
+# ECADnet_Decoder
+from ..ECA_dnet_decoder import ECADnet_DepthDecoder
+# Dnet_Decoder
+from ..dnet_decoder import Dnet_DepthDecoder
+# Original Decoder
+from ..depth_decoder import DepthDecoder
+
+
+class DepthRepVGGNet(nn.Module):
+    """
+    Inverse depth network based on the ResNet architecture.
+
+    Parameters
+    ----------
+    version : str
+        Has a XY format, where:
+        X is the number of residual layers [18, 34, 50] and
+        Y is an optional ImageNet pretrained flag added by the "pt" suffix
+        Example: "18pt" initializes a pretrained ResNet18, and "34" initializes a ResNet34 from scratch
+    kwargs : dict
+        Extra parameters
+    """
+
+    def __init__(self, version=None, **kwargs):
+        super().__init__()
+        assert version is not None, "DispResNet needs a version"
+
+        num_layers = int(version[:2])  # First two characters are the number of layers
+        if version[2:] == 'np':  #
+            pretrained = 'np'
+        elif version[2:] == 'pt':
+            pretrained = 'pt'
+            # If the last characters are "pt", use ImageNet original pretraining
+        else:
+            pretrained = False
+        assert num_layers in [18, 34, 50], 'ResNet version {} not available'.format(num_layers)
+
+        self.num_ch_enc = np.array([48, 48, 96, 192, 1280])
+        self.encoder = RepVGGencoder(pretrained=pretrained)
+        self.decoder = DepthDecoder(num_ch_enc=self.num_ch_enc)
+        self.scale_inv_depth = partial(disp_to_depth, min_depth=0.1, max_depth=80.0)
+
+    def forward(self, x):
+        """
+        Runs the network and returns inverse depth maps
+        (4 scales if training and 1 if not).
+        """
+        x = self.encoder(x)
+        x = self.decoder(x)
+        disps = [x[('disp', i)] for i in range(4)]
+        d_disp_0 = disps[0]
+
+        if self.training:
+            return [self.scale_inv_depth(d)[0] for d in disps]
+        else:
+            # return F.interpolate(self.scale_inv_depth(d_disp_0)[0], scale_factor=0.5)
+            return self.scale_inv_depth(disps[0])[0]
