444dbb189dafa0c2dfe340aa3425003c9009c4bf
seok436 <seok7302@gmail.com>
2023-03-09 21:36:58 +0900

    A5_dataset_split & minor update

---
+++ A5_dataset_splits.py
@@ -0,0 +1,42 @@
+import os
+from tqdm import tqdm
+from os import listdir
+from options import MonodepthOptions
+
+file_dir = os.path.dirname(__file__)  # the directory that A5_dataset_splits.py resides in
+
+def A5_dataset_split(opt):
+    train_val_folder = ['A5T_0001', 'A5T_0002', 'A5T_0003']
+    dataset_version = opt.data_path.split(os.path.sep)[-1]
+    concat_train_path = os.path.join(opt.data_path, train_val_folder[0]) # Train
+    concat_val_path = os.path.join(opt.data_path, train_val_folder[2]) # Validation
+    train_folder_list = sorted(listdir(concat_train_path))
+    val_folder_list = sorted(listdir(concat_val_path))
+    # Creating splits folder if it doesn't exists
+    db_version_split_path = os.path.join(os.path.join(file_dir, 'splits'), dataset_version)
+    os.makedirs(db_version_split_path, exist_ok=True)
+    train_txt_name, val_txt_name = 'train_files.txt', 'val_files.txt'
+    
+    # Making train files
+    f_t = open(os.path.join(db_version_split_path, train_txt_name), 'w')
+    for folder in tqdm(train_folder_list):
+        images = sorted([i for i in listdir(os.path.join(concat_train_path, folder)) if i.endswith('.jpg')])
+        for i in range(1, len(images)-2):
+            train_file = '{} {}\n'.format(os.path.join(train_val_folder[0], folder), os.path.splitext(images[i])[0])
+            f_t.write(train_file)
+    f_t.close()
+
+    # Making val files
+    f_v = open(os.path.join(db_version_split_path, val_txt_name), 'w')
+    for folder in tqdm(val_folder_list):
+        images = sorted([i for i in listdir(os.path.join(concat_val_path, folder)) if i.endswith('.jpg')])
+        for i in range(1, len(images)-2):
+            val_file = '{} {}\n'.format(os.path.join(train_val_folder[2], folder), os.path.splitext(images[i])[0])
+            f_v.write(val_file)
+    f_t.close()
+    print('Work is done.')
+
+
+if __name__ == "__main__":
+    options = MonodepthOptions()
+    A5_dataset_split(options.parse())
